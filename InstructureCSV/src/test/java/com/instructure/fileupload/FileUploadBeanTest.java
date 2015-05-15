393775aae8b41162eb811392320401b555ccbf2f
Rob Weaver <rob@accuweaver.com>
2015-05-14 21:37:47 -0600

    Add unit test for content scenarios of student, course and null byte arrays

--- InstructureCSV/src/test/java/com/instructure/fileupload/FileUploadBeanTest.java
+++ InstructureCSV/src/test/java/com/instructure/fileupload/FileUploadBeanTest.java
@@ -21,24 +21,36 @@ import org.primefaces.model.UploadedFile;
  * @author robweaver
  */
 public class FileUploadBeanTest {
-    private final String MOCK_FILE_NAME = "myFileName.csv";
-    private final Long MOCK_FILE_SIZE = 5000l;
+
+    private static final String MOCK_FILE_NAME = "myFileName.csv";
+    private static final Long MOCK_FILE_SIZE = 5000l;
+    private static UploadedFile mockFile = Mockito.mock(UploadedFile.class);
+    private FileUploadBean instance;
+    String studentHeader = "course_id, course_name, state";
+    private byte[] studentHeaderBytes = (studentHeader).getBytes();
+    String courseHeader = "user_id, user_name, course_id, state";
+    private byte[] courseHeaderBytes = (courseHeader).getBytes();
     
     public FileUploadBeanTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
+        mockFile = Mockito.mock(UploadedFile.class);
+        when(mockFile.getFileName()).thenReturn(MOCK_FILE_NAME);
+        when(mockFile.getSize()).thenReturn(MOCK_FILE_SIZE);
+
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
+        instance = new FileUploadBean();
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -53,7 +65,7 @@ public class FileUploadBeanTest {
         UploadedFile expResult = null;
         UploadedFile result = instance.getFile();
         assertEquals(expResult, result);
-      
+
     }
 
     /**
@@ -65,7 +77,7 @@ public class FileUploadBeanTest {
         UploadedFile file = null;
         FileUploadBean instance = new FileUploadBean();
         instance.setFile(file);
-        
+
     }
 
     /**
@@ -74,27 +86,58 @@ public class FileUploadBeanTest {
     @Test
     public void testFileUploadListener() {
         System.out.println("fileUploadListener");
-        UploadedFile mockFile = Mockito.mock(UploadedFile.class);
-        when(mockFile.getFileName()).thenReturn(MOCK_FILE_NAME);
-        when(mockFile.getSize()).thenReturn(MOCK_FILE_SIZE);
+
         FileUploadEvent e = Mockito.mock(FileUploadEvent.class);
         when(e.getFile()).thenReturn(mockFile);
-        FileUploadBean instance = new FileUploadBean();
         instance.fileUploadListener(e);
-        
+
     }
 
     /**
-     * Test of isFileStudent method, of class FileUploadBean.
+     * Test of isFileStudent method with student content, of class FileUploadBean.
+     * 
+     * Should return true
      */
     @Test
     public void testIsFileStudent() {
         System.out.println("isFileStudent");
-        FileUploadBean instance = new FileUploadBean();
-        boolean expResult = false;
-        boolean result = instance.isFileStudent();
+        when(mockFile.getContents()).thenReturn(studentHeaderBytes);
+        boolean expResult = true;
+        boolean result = instance.isFileStudent(); 
         assertEquals(expResult, result);
-        
+
+    }
+
+    /**
+     * Test of isFileStudent method for course content, of class FileUploadBean.
+     * 
+     * Should return false
+     */
+    @Test
+    public void testIsFileStudentCourse() {
+        System.out.println("isFileStudent");
+        when(mockFile.getContents()).thenReturn(courseHeaderBytes);
+        boolean expResult = false;
+        boolean result = instance.isFileStudent(); 
+        assertEquals(expResult, result);
+
+    }
+
+
+    /**
+     * Test of isFileStudent method for course content, of class FileUploadBean.
+     * 
+     * Should return false
+     */
+    @Test
+    public void testIsFileStudentNoContent() {
+        System.out.println("isFileStudent");
+        byte[] emptyByteArray = null;
+        when(mockFile.getContents()).thenReturn(emptyByteArray);
+        boolean expResult = false;
+        boolean result = instance.isFileStudent(); 
+        assertEquals(expResult, result);
+
     }
 
     /**
@@ -103,9 +146,9 @@ public class FileUploadBeanTest {
     @Test
     public void testProcessStudents() {
         System.out.println("processStudents");
-        FileUploadBean instance = new FileUploadBean();
+        
         instance.processStudents();
-       
+
     }
 
     /**
@@ -116,7 +159,7 @@ public class FileUploadBeanTest {
         System.out.println("processCourses");
         FileUploadBean instance = new FileUploadBean();
         instance.processCourses();
-       
+
     }
-    
+
 }
