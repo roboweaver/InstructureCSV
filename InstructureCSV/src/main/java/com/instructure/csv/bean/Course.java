7d4184ca77a092c724c767e0fee7c7b150b3f406
Rob Weaver <rob@accuweaver.com>
2015-05-15 20:06:18 -0600

    Base Course and Student bean

---
+++ InstructureCSV/src/main/java/com/instructure/csv/bean/Course.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.instructure.csv.bean;
+
+import java.io.Serializable;
+import org.csveed.annotations.CsvCell;
+import org.csveed.annotations.CsvFile;
+
+/**
+ *
+ * @author robweaver
+ */
+@CsvFile(comment = '%', quote='\'', escape='\\', separator=',', useHeader = true)
+public class Course implements Serializable {
+    
+    @CsvCell(columnIndex = 0, required = true)
+    private Integer courseId;
+    @CsvCell(columnIndex = 1)
+    private String courseName;
+    @CsvCell(columnIndex = 2)
+    private String active;
+
+    /**
+     * @return the courseId
+     */
+    public Integer getCourseId() {
+        return courseId;
+    }
+
+    /**
+     * @param courseId the courseId to set
+     */
+    public void setCourseId(Integer courseId) {
+        this.courseId = courseId;
+    }
+
+    /**
+     * @return the courseName
+     */
+    public String getCourseName() {
+        return courseName;
+    }
+
+    /**
+     * @param courseName the courseName to set
+     */
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    /**
+     * @return the active
+     */
+    public String getActive() {
+        return active;
+    }
+
+    /**
+     * @param active the active to set
+     */
+    public void setActive(String active) {
+        this.active = active;
+    }
+    
+    
+}
---
+++ InstructureCSV/src/main/java/com/instructure/csv/bean/Student.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.instructure.csv.bean;
+
+import java.io.Serializable;
+import org.csveed.annotations.CsvCell;
+import org.csveed.annotations.CsvFile;
+
+/**
+ *
+ * @author robweaver
+ */
+@CsvFile(comment = '%', quote = '\'', escape = '\\', separator = ',')
+public class Student implements Serializable {
+
+    @CsvCell(columnIndex = 0, required = true)
+    private Integer userId;
+    @CsvCell(columnIndex = 1)
+    private String userName;
+    @CsvCell(columnIndex = 2)
+    private Integer courseId;
+    @CsvCell(columnIndex = 4)
+    private String active;
+
+    /**
+     * @return the userId
+     */
+    public Integer getUserId() {
+        return userId;
+    }
+
+    /**
+     * @param userId the userId to set
+     */
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    /**
+     * @return the userName
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * @param userName the userName to set
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    /**
+     * @return the courseId
+     */
+    public Integer getCourseId() {
+        return courseId;
+    }
+
+    /**
+     * @param courseId the courseId to set
+     */
+    public void setCourseId(Integer courseId) {
+        this.courseId = courseId;
+    }
+
+    /**
+     * @return the active
+     */
+    public String getActive() {
+        return active;
+    }
+
+    /**
+     * @param active the active to set
+     */
+    public void setActive(String active) {
+        this.active = active;
+    }
+
+}
